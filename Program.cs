2dbc71bb6378a52aeefc32182b7a11cfcbd5fac2
Daniel Ramos <daniel@codemonk.eu>
2026-03-03 15:12:50 +0000

    Add missing STAThread attribute to fix DataGridView ComboBox error.

--- Program.cs
+++ Program.cs
@@ -3,27 +3,34 @@ using System.Security.Principal;
 using TrayCommander;
 using TrayCommander.Services;
 
-Application.SetHighDpiMode(HighDpiMode.SystemAware);
-Application.EnableVisualStyles();
-Application.SetCompatibleTextRenderingDefault(false);
-
-static bool IsElevated()
+internal static class Program
 {
-    using var identity = WindowsIdentity.GetCurrent();
-    return new WindowsPrincipal(identity).IsInRole(WindowsBuiltInRole.Administrator);
-}
-
-var options = PersistenceService.LoadOptions();
-var isElevated = IsElevated();
-
-if (options.AlwaysRunAsAdmin && !isElevated)
-{
-    var exe = Process.GetCurrentProcess().MainModule?.FileName;
-    if (exe != null)
+    [STAThread]
+    static void Main()
     {
-        Process.Start(new ProcessStartInfo(exe) { Verb = "runas", UseShellExecute = true });
-        return;
+        Application.SetHighDpiMode(HighDpiMode.SystemAware);
+        Application.EnableVisualStyles();
+        Application.SetCompatibleTextRenderingDefault(false);
+
+        var options = PersistenceService.LoadOptions();
+        var isElevated = IsElevated();
+
+        if (options.AlwaysRunAsAdmin && !isElevated)
+        {
+            var exe = Process.GetCurrentProcess().MainModule?.FileName;
+            if (exe != null)
+            {
+                Process.Start(new ProcessStartInfo(exe) { Verb = "runas", UseShellExecute = true });
+                return;
+            }
+        }
+
+        Application.Run(new TrayApp(isElevated));
+    }
+
+    static bool IsElevated()
+    {
+        using var identity = WindowsIdentity.GetCurrent();
+        return new WindowsPrincipal(identity).IsInRole(WindowsBuiltInRole.Administrator);
     }
 }
-
-Application.Run(new TrayApp(isElevated));
